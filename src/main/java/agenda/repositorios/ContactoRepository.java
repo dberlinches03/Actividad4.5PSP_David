30b931b93202be0939ce4a43576adf612e55ee5b
dberl <dberlinches2003@gmail.com>
2026-01-28 23:29:03 +0100

    Initial commit

---
+++ src/main/java/agenda/repositorios/ContactoRepository.java
@@ -0,0 +1,12 @@
+package agenda.repositorios;
+
+import agenda.entidades.Contacto;
+
+import java.util.List;
+
+public interface ContactoRepository {
+    List<Contacto> obtenerTodos();
+    Contacto obtenerPorId(Long id);
+    Contacto guardar(Contacto contacto);
+    void eliminar(Long id);
+}
---
+++ src/main/java/agenda/servicios/ContactoService.java
@@ -0,0 +1,12 @@
+package agenda.servicios;
+
+import agenda.entidades.Contacto;
+
+import java.util.List;
+
+public interface ContactoService {
+    List<Contacto> obtenerTodos();
+    Contacto obtenerPorId(Long id);
+    Contacto guardar(Contacto contacto);
+    void eliminar(Long id);
+}
